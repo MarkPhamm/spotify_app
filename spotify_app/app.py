69dff874dd93ce80defb4882f9603eedea81d353
Kaylee Fischer <kaylee.fischer@tcu.edu>
2024-05-02 14:21:10 -0500

    corrected grammar

--- spotify_app/app.py
+++ spotify_app/app.py
@@ -53,8 +53,8 @@ def return_oldest_album(all_time_df):
 def return_artist_appearance_in_all_time(user_artist, all_time_df):
     number_of_appearance = len(all_time_df.loc[all_time_df['artist_name'] == user_artist])
     if number_of_appearance == 0:
-        return("Unique Choice, your artist have never appear in the all-time list")
-    return(F"Wow, your artist has {number_of_appearance} songs in the all time list")
+        return("Unique Choice, your artist has never appeared on the all-time list.")
+    return(F"Wow, your artist has {number_of_appearance} songs on the all time list.")
 
 def return_artist_songs_appearance_in_all_time(user_artist, all_time_df):
     return all_time_df.loc[all_time_df['artist_name'] == user_artist][['song_name']].to_html()
