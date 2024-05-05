772fe8beb6cb219a8c61eb655aa43015bb76512b
Kaylee Fischer <kaylee.fischer@tcu.edu>
2024-05-05 17:30:43 -0500

    change table style

--- spotify_app/jackson.py
+++ spotify_app/jackson.py
@@ -3,7 +3,7 @@ import pandas as pd
 def return_albums_in_all_time_list(artist_album_df, all_time_album_df):
     merge = pd.merge(artist_album_df, all_time_album_df, how='inner', left_on = 'AlbumName', right_on = 'album_name')
     merge = merge.drop_duplicates(subset=['AlbumName'])
-    return merge[['album_id', 'album_name', 'release_date','total_tracks', 'url','album_old']].to_html()
+    return merge[['album_name', 'release_date','total_tracks', 'url']].to_html(index=False)
 
 def return_number_of_album_in_all_time_list(artist_album_df, all_time_album_df):
     merge = pd.merge(artist_album_df, all_time_album_df, how='inner', left_on = 'AlbumName', right_on = 'album_name')
